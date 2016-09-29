774d9914e3e80733db422564f1bf0a197379634e
zoolsher <zoolsher@gmail.com>
2016-09-30 00:39:07 +0800

    add uncaughtException handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import ConfigData from './../config';
 
 /**
@@ -18,11 +19,14 @@ var channelManager = ChannelManager.getInstance()
 var mainChannel = channelManager.get("main");
 global.app.ChannelManager = ChannelManager;
 global.app.Logger = Util.Logger;
+process.on('uncaughtException',err=>{
+    Logger.error(err);
+})
 /**
  * register the message event on the messageQueen
  */
 sock.on("message", function (data) {
-    var action = {};
+    var action = {};// {type:CREATED_A_NEW_PROJECT,params:{id:adsfsdfasdfqf2343}}
     try {
         action = JSON.parse(data);
         mainChannel.push(action);
@@ -33,3 +37,5 @@ sock.on("message", function (data) {
 
 global.app.controllers = [];
 global.app.controllers.push(require("./controller/MainController"));
+Util.Logger.info("service loaded MainController");
+Util.Logger.info("service is running");
