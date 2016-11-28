bf6b8949dddac3bf2e751687ca496886e95b8bbc
zoolsher <zoolsher@gmail.com>
2016-11-28 16:04:17 +0800

    no message

--- src/model/index.js
+++ src/model/index.js
@@ -6,14 +6,14 @@ import ProjectSchema from './ProjectSchema';
 mongoose.connect(global.app.configData.db.db_url);
 mongoose.Promise = require('bluebird');
 var db = mongoose.connection;
-db.on('error',function(){
+db.on('error', function () {
     logger.error("connection error");
 })
 
-db.once('open',function(){
+db.once('open', function () {
     logger.info("connect mongodb success");
 })
-
-export var Log = mongoose.model('log',LogSchema);
-export var Project = mongoose.model('projects',ProjectSchema);
+//zhe ge shen qi le
+export var Log = mongoose.model('log', LogSchema);
+export var Project = mongoose.model('projects', ProjectSchema);
 export default db;
